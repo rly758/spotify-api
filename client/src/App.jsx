5095c39ea06362eaa9b2da2c89d84347869973b2
Richard Ly <rly758@gmail.com>
2024-09-04 13:29:04 -0700

    Tested proxy. Implemented API access token retrieval.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Home from "./Home";
+import Loading from "./Loading";
+import Recommender from "./Recommender";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [auth, setAuth] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    axios.get("/auth/current-session").then(({ data }) => {
+      setAuth(data);
+    });
+  }, []);
+
+  if (auth === null) {
+    return <Loading />;
+  }
+  if (auth) {
+    return <Recommender auth={auth} />;
+  }
+  return <Home />;
 }
 
-export default App
+export default App;
---
+++ client/src/Recommender.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Recommender = () => {
+  return <div>I'm logged in</div>;
+};
+export default Recommender;

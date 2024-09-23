b75ce458832f10ccd819924685878c1419cc67a4
Richard Ly <rly758@gmail.com>
2024-09-22 21:08:26 -0700

    Progress on slider board component for track traits

--- client/src/Recommender.jsx
+++ client/src/Recommender.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
-import { Grid2 as Grid, TextField, Button } from "@mui/material";
+import { Grid2 as Grid, TextField, Button, Typography } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import SearchResults from "./components/SearchResults";
+import TrackList from "./components/TrackList";
+import SliderBoard from "./components/SliderBoard";
 
 const Recommender = ({ auth }) => {
-  const { token } = auth;
-  const [searchString, setSearchString] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [selectedArtists, setSelectedArtists] = useState([]);
+  const { token } = auth; //api access token
+  const [searchString, setSearchString] = useState(""); //search bar string
+  const [searchResults, setSearchResults] = useState([]); //search results from search bar
+  const [selectedArtists, setSelectedArtists] = useState([]); //selected seed artists for recommending
+  const [sliderValues, setSliderValues] = useState({}); //floor and ceiling track trait values for recommending
+  const [recommendationResults, setRecommendationResults] = useState(null); //recommended tracks given slider values, seed artists, and/or seed genres
 
   const searchSpotify = async () => {
     const url = "https://api.spotify.com/v1/search";
@@ -22,11 +26,53 @@ const Recommender = ({ auth }) => {
       .then((data) => setSearchResults(data.artists.items)); //console.log(searchResults) will give current render's state value, not the updated value
   };
 
+  const getRecommendations = async () => {
+    const url = "https://api.spotify.com/v1/recommendations";
+
+    // create artists query string
+    let selectedArtistsString;
+    if (selectedArtists.length < 0) {
+      return;
+    } else {
+      selectedArtistsString = `seed_artists=${selectedArtists.join(",")}`;
+    }
+
+    // create sliders query string
+    let min = [];
+    let max = [];
+    Object.keys(sliderValues).forEach((slider) => {
+      if (sliderValues[slider].enabled) {
+        // then we add our min and max values
+        min.push(`min_${slider}=${sliderValues[slider].value[0]}`);
+        max.push(`max_${slider}=${sliderValues[slider].value[1]}`);
+      }
+    });
+    const minString = min.join("&");
+    const maxString = max.join("&");
+
+    console.log(`${url}?${selectedArtistsString}&${minString}&${maxString}`);
+
+    fetch(`${url}?${selectedArtistsString}&${minString}&${maxString}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setRecommendationResults(data));
+  };
+
   return (
     <div className="Recommender">
       <Grid container style={{ padding: 20 }} spacing={1}>
         <Grid size={{ xs: 12 }}>Spotify Recommender</Grid>
         <Grid size={{ xs: 6 }}>
+          <Grid size={{ xs: 12 }}>
+            {selectedArtists.map((artist, index) => (
+              <Typography>
+                {index + 1}. {artist}
+              </Typography>
+            ))}
+          </Grid>
           <Grid
             size={{ xs: 12 }}
             style={{ display: "flex", flexDirection: "row" }}
@@ -46,12 +92,25 @@ const Recommender = ({ auth }) => {
               <Search />
             </Button>
           </Grid>
-          <Grid xs={12}>
+          <Grid size={{ xs: 12 }}>
             <SearchResults
               onChange={setSelectedArtists}
               results={searchResults}
             />
           </Grid>
+        </Grid>
+        <Grid size={{ xs: 6 }}>
+          <SliderBoard onChange={setSliderValues} />
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          <Button variant={"contained"} onClick={getRecommendations}>
+            Get Recommendations
+          </Button>
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          {recommendationResults && (
+            <TrackList results={recommendationResults} />
+          )}
         </Grid>
       </Grid>
     </div>
---
+++ client/src/components/SliderBoard.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import { Grid2 as Grid, Typography, Slider, Checkbox } from "@mui/material";
+
+const sliders = {
+  acousticness: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+  danceability: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+  energy: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+  instrumentalness: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+  liveness: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+  loudness: {
+    value: [-60, 0],
+    enabled: false,
+    min: -60,
+    max: 0,
+  },
+  popularity: {
+    value: [0, 100],
+    enabled: false,
+    min: 0,
+    max: 100,
+    step: 1,
+  },
+  speechiness: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+  tempo: {
+    value: [0, 200],
+    enabled: false,
+    min: 0,
+    max: 200,
+    step: 1,
+  },
+  valence: {
+    value: [0, 1],
+    enabled: false,
+    min: 0,
+    max: 1,
+    step: 0.01,
+  },
+};
+
+const SliderBoard = ({ onChange }) => {
+  const [sliderValues, setsliderValues] = useState(sliders);
+
+  const handleChange = (slider, value) => {
+    const newsliderValues = { ...sliderValues };
+    newsliderValues[slider].value = value;
+    setsliderValues(newsliderValues);
+  };
+
+  const toggleSlider = (slider, value) => {
+    const newsliderValues = { ...sliderValues };
+    newsliderValues[slider].enabled = value;
+    setsliderValues(newsliderValues);
+  };
+
+  useEffect(() => {
+    onChange(sliderValues);
+  }, [onChange, sliderValues]);
+
+  return (
+    <Grid container spacing={2} style={{ padding: 10 }}>
+      {Object.keys(sliders).map((slider) => (
+        <Grid size={{ xs: 12 }} key={slider}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Checkbox
+              checked={sliderValues[slider].enabled}
+              onChange={(event, newValue) => toggleSlider(slider, newValue)}
+            />
+            <div style={{ flex: 1 }}>
+              <Typography>{slider}</Typography>
+              <Grid container spacing={1}>
+                <Grid>
+                  <Typography>Min</Typography>
+                </Grid>
+                <Grid>
+                  <Slider
+                    disabled={!sliderValues[slider].enabled}
+                    value={sliderValues[slider].value}
+                    onChange={(event, newValue) =>
+                      handleChange(slider, newValue)
+                    }
+                    valueLabelDisplay={"on"}
+                    aria-labelledby={"range-slider"}
+                    min={sliders[slider].min}
+                    max={sliders[slider].max}
+                    step={sliders[slider].step}
+                  />
+                </Grid>
+                <Grid>
+                  <Typography>Max</Typography>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default SliderBoard;
